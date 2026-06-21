3daab02cb911c35ecae83c28d5060fb8cb287305
SeaExculbur <174923904+SeaExculbur@users.noreply.github.com>
2026-06-21 22:02:51 +0800

    feat: 登录页 HTML 骨架

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -2,5 +2,19 @@
 </script>
 
 <template>
-  <div>登录页</div>
+<div>
+  <label>用户名：</label>
+  <input type="text" placeholder="请输入用户名">
+</div>
+<div>
+  <label for="pwd">密码：</label>
+  <input type="password" id="pwd" placeholder="请输入密码">
+</div>
+<div>
+  <button>登录</button>
+</div>
+<div>
+  <a href="#">还没有注册？去注册</a>
+</div>
+
 </template>
